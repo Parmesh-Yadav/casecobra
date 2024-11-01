a3d020c962724044dc45f01386165e9e0acaf1db
PARMESH YADAV <parmesh20319@iiitd.ac.in>
2024-11-01 18:36:01 +0530

    fix kinde auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import {Recursive} from "next/font/google";
+import { Recursive } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
 import { constructMetadata } from "@/lib/utils";
+import { AuthProvider } from "./AuthProvider";
 
-const recursive = Recursive({subsets:['latin']})
+const recursive = Recursive({ subsets: ['latin'] })
 
 export const metadata = constructMetadata();
 
@@ -17,21 +18,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={recursive.className}
-      >
-        <NavBar />
-        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)] grainy-light">
-          <div className="flex-1 flex flex-col h-full bg-white">
-            <Providers>
-              {children}
-            </Providers>
-          </div>
-          <Footer />
-        </main>
-        <Toaster />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body
+          className={recursive.className}
+        >
+          <NavBar />
+          <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)] grainy-light">
+            <div className="flex-1 flex flex-col h-full bg-white">
+              <Providers>
+                {children}
+              </Providers>
+            </div>
+            <Footer />
+          </main>
+          <Toaster />
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
